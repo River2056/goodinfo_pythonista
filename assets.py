76c6da7b8232c7fd30d6c5a1a4ed8102c31c8d84
kevin.tung <chen0625tung@gmail.com>
2021-12-08 13:07:10 +0800

    modified asset query sql

--- assets.py
+++ assets.py
@@ -13,9 +13,9 @@ def query_assets():
             A.AVERAGE_PRICE,
             A.COST_OF_INVESTMENT,
             DP.CURRENT_PRICE,
-            DP.CURRENT_PRICE * A.STOCK_HOLDINGS AS BOOK_VALUE,
-            (A.STOCK_HOLDINGS * DP.CURRENT_PRICE) - A.COST_OF_INVESTMENT AS PROFIT_AND_LOSS,
-            (((A.STOCK_HOLDINGS * DP.CURRENT_PRICE) - A.COST_OF_INVESTMENT) / A.COST_OF_INVESTMENT) * 100 AS PROFIT_AND_LOSS_PERCENTAGE
+            (DP.CURRENT_PRICE * A.STOCK_HOLDINGS) - ((DP.CURRENT_PRICE * A.STOCK_HOLDINGS) * 0.001425) - ((DP.CURRENT_PRICE * A.STOCK_HOLDINGS) * 0.003) AS BOOK_VALUE,
+            (A.STOCK_HOLDINGS * DP.CURRENT_PRICE) - A.COST_OF_INVESTMENT - ((DP.CURRENT_PRICE * A.STOCK_HOLDINGS) * 0.001425) - ((DP.CURRENT_PRICE * A.STOCK_HOLDINGS) * 0.003) AS PROFIT_AND_LOSS,
+            (((A.STOCK_HOLDINGS * DP.CURRENT_PRICE) - A.COST_OF_INVESTMENT - ((DP.CURRENT_PRICE * A.STOCK_HOLDINGS) * 0.001425) - ((DP.CURRENT_PRICE * A.STOCK_HOLDINGS) * 0.003)) / A.COST_OF_INVESTMENT) * 100 AS PROFIT_AND_LOSS_PERCENTAGE
         FROM ASSETS A
         JOIN DAILY_PRICE DP ON A.ID = DP.ID;
     """)
@@ -23,15 +23,15 @@ def query_assets():
     print('{:<10}\t{:<10}\t{:<10}\t{:<10}\t{:<10}\t{:<10}\t{:<10}\t{:<10}'.format("代號","持股","成交均價","投資成本","市價","帳面市值","損益","損益率%"))
     for row in rows:
         print(
-            '{id:<10}\t{holdings:<10}\t{avg_price:<10}\t{cost:<10}\t{curr:<10}\t{book:<10}\t{profit:<10}\t{profit_p:<10.2f}'
+            '{id:<10}\t{holdings:<10}\t{avg_price:<10}\t{cost:<10}\t{curr:<10}\t{book:<10.2f}\t{profit:<10.2f}\t{profit_p:<10.2f}'
             .format(
                 id=row[1],
                 holdings=row[3],
                 avg_price=row[4],
                 cost=row[5],
                 curr=row[6],
-                book=row[7],
-                profit=row[8],
+                book=float(row[7]),
+                profit=float(row[8]),
                 profit_p=float(row[9])
             )
         )
